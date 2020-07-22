2e60bd58c4a849f40fb08535a81801022f47926d
liencard <lien.cardoen@student.howest.be>
2020-07-22 13:54:51 +0200

    start files

---
+++ website-bikedata/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Switch, Route } from 'react-router-dom';
+// import { ROUTES } from './const/index';
+// import style from "./App.module.css";
+
+function App() {
+    return (
+        <>
+            <p>test</p>
+        </>
+    );
+}
+
+export default App;
