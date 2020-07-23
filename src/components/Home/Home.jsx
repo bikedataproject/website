d05b13b3acba171b4a5f0f0dc8384e495d55f3ae
liencard <lien.cardoen@student.howest.be>
2020-07-23 10:21:32 +0200

    further added content home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,8 +13,8 @@ const Home = () => {
           <p>
             With this project we aim to create a open data platform, for and by
             cyclists, that collects data on where and when people actually
-            cycle. The platform aims at aggregating data from cyclists , to
-            make cyclists more visible and cities better adapted to the cycling
+            cycle. The platform aims at aggregating data from cyclists , to make
+            cyclists more visible and cities better adapted to the cycling
             community.
           </p>
           <p className={style.intro__counter}>
@@ -24,11 +24,13 @@ const Home = () => {
         </div>
       </section>
 
-      <section className={style.content}>
+      <section className={`${style.content} ${style.grid}`}>
         <div className={style.content__wrapper}>
           <h2 className={style.content__title}>How to help?</h2>
           <div className={style.content__text}>
-            <p>To make this work we need to ask you for a simple favor. </p>
+            <p className={style.content__textBold}>
+              To make this work we need to ask you for a simple favor.{' '}
+            </p>
             <p>
               By sharing your own cycling data to our platform, all the
               collected data gives us patterns that can be used to make cyclists
@@ -50,6 +52,79 @@ const Home = () => {
           width="468"
           height="593"
         />
+      </section>
+
+      <section className={`${style.content} ${style.grid}`}>
+        <div className={style.content__wrapper}>
+          <h2 className={style.content__title}>Donate your bike data</h2>
+          <div className={style.content__text}>
+            <p>
+              There are a several ways you can contribute data to the project.
+              If you already use one of the apps listed below you can easily
+              connect them to The Bike Data Project. Once your app is connected
+              you can just carry on as usual and your rides will automatically
+              be uploaded to our service.
+            </p>
+            <p>
+              You’re not using such an application yet? No worries! You can
+              still be part of this project by also downloading and using our
+              app.
+            </p>
+          </div>
+        </div>
+        <div className={style.content__buttons}>
+          <div className={style.buttons__other}>
+            <p>Connect your existing app</p>
+            <button>Strava</button>
+          </div>
+          <div className={style.buttons__our}>
+            <p>Download our app</p>
+            <button>Google Store</button>
+          </div>
+        </div>
+      </section>
+
+      <section className={style.content}>
+        <h2 className={style.content__title}>Collected data worldwide</h2>
+        <div className={`${style.data__overview} ${style.grid}`}>
+          <div className={style.data__set}>
+            <span className={style.data__number}>22.982</span>
+            <span className={style.data__label}>rides collected</span>
+          </div>
+          <div className={style.data__set}>
+            <span className={style.data__number}>
+              205.250 <span className={style.data__small}>km</span>
+            </span>
+            <span className={style.data__label}>rides collected</span>
+          </div>
+          <div className={style.data__set}>
+            <span className={style.data__number}>
+              25 <span className={style.data__small}>min</span>
+            </span>
+            <span className={style.data__label}>rides collected</span>
+          </div>
+          <div className={style.data__set}>
+            <span className={style.data__number}>
+              13.48 <span className={style.data__small}>km/h</span>
+            </span>
+            <span className={style.data__label}>rides collected</span>
+          </div>
+          <div className={style.data__set}>
+            <span className={style.data__number}>
+              5.72 <span className={style.data__small}>km</span>
+            </span>
+            <span className={style.data__label}>rides collected</span>
+          </div>
+          <div className={style.data__set}>
+            <span className={style.data__number}>
+              26.68 <span className={style.data__small}>km</span>
+            </span>
+            <span className={style.data__label}>rides collected</span>
+          </div>
+        </div>
+
+        <h3 className={style.subtitle}>Interested in the data by region?</h3>
+        <button>See full map</button>
       </section>
     </>
   );
